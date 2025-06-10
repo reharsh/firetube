640eade0104f94b8f412d5324050adab954ef5c8
Harsh <116981900+reharsh@users.noreply.github.com>
2025-06-10 23:06:10 +0530

    waitlist page added

---
+++ app/hero/page.tsx
@@ -0,0 +1,9 @@
+import {HeroGeometric} from "@/components/landing"
+
+export default function Demo (){
+    return (
+        <div className="flex items-center justify-center h-screen">
+        <HeroGeometric badge="newly launched" title1="Convert anything into" title2="Fireship videos"/>
+      </div> 
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { HeroGeometric } from "@/components/landing";
+import { HeroSection } from "@/components/hero";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <HeroGeometric badge="newly launched" title1="Convert anything into" title2="Fireship videos"/>
-    </div>
+    <HeroSection />
   );
 }
